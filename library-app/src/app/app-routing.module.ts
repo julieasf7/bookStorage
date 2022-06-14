a1e2e946ee492ce28a383675bd677f064123a322
Julie Sarmiento <ja.sarmiento@uniandes.edu.co>
2022-06-14 17:54:57 -0500

    Se configura routes para el home

--- library-app/src/app/app-routing.module.ts
+++ library-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { BooksHomeComponent } from './components/books-home/books-home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BooksHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
